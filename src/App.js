d0ef89b7a0f3d6b2d19bd5a5e1e1d231375883b2
Mina badrous <mina.badrous@dlivr.io>
2021-04-26 10:10:01 +0200

    Add another test modification

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>HELLO</h1>
+        <h1>NEW ONE</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
