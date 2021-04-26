075f2566f85286c845e8f218536194b5dc9ef720
Mina badrous <mina.badrous@dlivr.io>
2021-04-26 14:55:22 +0200

    Add files with standard lint fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React from "react"
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
+  console.log(0 === 0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <h1>HELLO</h1>
         <h1>NEW ONE</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
